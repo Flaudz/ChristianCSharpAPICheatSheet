1ee095c42cd3422015144ff513bb91c252cd4beb
Flaudz <nico936d@edu.campusvejle.dk>
2020-09-16 10:41:06 +0200

    Made The Api Stuff;)

---
+++ ApiRequest.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Net;
+using System.Text;
+using static ChristianCSharpAPICheatSheet.Movie;
+
+namespace ChristianCSharpAPICheatSheet
+{
+    public class ApiRequest
+    {
+        public Root GetMovies()
+        {
+            string json = new WebClient().DownloadString(@"https://api.themoviedb.org/3/search/movie?api_key=0a34d1c5f077444133ab367be386561a&language=en-US&query=Iron-Man&page=1&include_adult=false");
+            Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(json);
+            return myDeserializedClass;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.Design.Serialization;
+using static ChristianCSharpAPICheatSheet.Movie;
 
 namespace ChristianCSharpAPICheatSheet
 {
@@ -6,7 +8,20 @@ namespace ChristianCSharpAPICheatSheet
     {
         static void Main(string[] args)
         {
-            
+            movieRquest();
+        }
+
+        public static void movieRquest()
+        {
+            Console.Clear();
+
+            ApiRequest apiRequest = new ApiRequest();
+            Root Movie = apiRequest.GetMovies();
+            foreach (var item in Movie.results)
+            {
+                Console.WriteLine($"Title: {item.original_title}");
+            }
+            Console.ReadLine();
         }
     }
 }
